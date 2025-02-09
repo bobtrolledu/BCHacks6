547a830998af34b3312fdb36e8f80c8c9e7d5153
Victor <victoryang2005@outlook.com>
2025-02-08 23:52:39 -0800

    landing page update to different pages secitons

---
+++ BCHacks/main/src/LandingPage.js
@@ -0,0 +1,50 @@
+import Orb from "./blocks/Backgrounds/Orb/Orb";
+import GradientText from "./blocks/TextAnimations/GradientText/GradientText";
+import React from "react";
+import './LandingPage.css';
+
+
+function LandingPage() {
+    return (
+        <div
+            className="HeroSplash"
+            style={{
+                backgroundColor: '#000',
+                width: '100%',
+                minHeight: '100vh',
+                position: 'relative',
+            }}
+        >
+            {/* Top section: Orb with centered gradient text */}
+            <div style={{width: '100%', height: '90vh', position: 'relative'}}>
+                <Orb
+                    hoverIntensity={3.0}
+                    rotateOnHover={true}
+                    hue={0}
+                    forceHoverState={false}
+                />
+
+                <div
+                    style={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        zIndex: 1, // Ensures the text is on top of the orb
+                    }}
+                >
+                    <GradientText
+                        colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+                        animationSpeed={3}
+                        showBorder={false}
+                        className="hero-text"
+                    >
+                        chromesthesia
+                    </GradientText>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage;
--- BCHacks/main/src/index.js
+++ BCHacks/main/src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Dock from './blocks/Components/Dock/Dock';
-import Orb from "./blocks/Backgrounds/Orb/Orb";
-import GradientText from "./blocks/TextAnimations/GradientText/GradientText";
 import { HiOutlineHome } from "react-icons/hi";
 import { HiOutlineCog } from "react-icons/hi";
 import { HiOutlineFolder } from "react-icons/hi";
 import { HiOutlineDocument } from "react-icons/hi";
+import LandingPage from "./LandingPage";
 
 
 
@@ -22,48 +21,7 @@ const items = [
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <div
-            style={{
-                backgroundColor: '#000',
-                width: '100%',
-                minHeight: '100vh',
-                position: 'relative',
-            }}
-        >
-            {/* Top section: Orb with centered gradient text */}
-            <div style={{ width: '100%', height: '90vh', position: 'relative' }}>
-                <Orb
-                    hoverIntensity={3.0}
-                    rotateOnHover={true}
-                    hue={0}
-                    forceHoverState={false}
-                />
-
-                <div
-                    style={{
-                        position: 'absolute',
-                        top: '50%',
-                        left: '50%',
-                        transform: 'translate(-50%, -50%)',
-                        zIndex: 1, // Ensures the text is on top of the orb
-                    }}
-                >
-                    <GradientText
-                        colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
-                        animationSpeed={3}
-                        showBorder={false}
-                        className="custom-class"
-                    >
-                        chromesthesia
-                    </GradientText>
-                </div>
-            </div>
-
-            {/* Bottom section: Dock */}
-
-
-
-        </div>
+        <LandingPage/>
         <Dock
             items={items}
             panelHeight={25}
