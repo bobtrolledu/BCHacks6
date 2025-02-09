277daaa5c5924f468920391d56fe6aec6275715d
Victor <victoryang2005@outlook.com>
2025-02-09 10:38:37 -0800

    fix canvas layout

--- BCHacks/main/src/Pages/Canvas.js
+++ BCHacks/main/src/Pages/Canvas.js
@@ -61,6 +61,7 @@ const Canvas = () => {
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "center",
+                flexDirection: "column",
 
             }}
         >
