84a6cce3d6e5d4a71e08c92f1986d06d46d5bbab
kiellg <kielgaol23@gmail.com>
2025-02-08 23:43:16 -0800

    landing page

--- BCHacks/main/src/index.js
+++ BCHacks/main/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import Dock from './blocks/Components/Dock/Dock';
 import Orb from "./blocks/Backgrounds/Orb/Orb";
+import GradientText from "./blocks/TextAnimations/GradientText/GradientText";
 import { HiOutlineHome } from "react-icons/hi";
 import { HiOutlineCog } from "react-icons/hi";
 import { HiOutlineFolder } from "react-icons/hi";
@@ -21,20 +22,53 @@ const items = [
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        <div
+            style={{
+                backgroundColor: '#000',
+                width: '100%',
+                minHeight: '100vh',
+                position: 'relative',
+            }}
+        >
+            {/* Top section: Orb with centered gradient text */}
+            <div style={{ width: '100%', height: '90vh', position: 'relative' }}>
+                <Orb
+                    hoverIntensity={3.0}
+                    rotateOnHover={true}
+                    hue={0}
+                    forceHoverState={false}
+                />
 
-        <div style={{width: '100%', height: '100vh', position: 'relative'}}>
-            <Orb
-                hoverIntensity={3}
-                rotateOnHover={true}
-                hue={254}
-                forceHoverState={false}
-            />
+                <div
+                    style={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        zIndex: 1, // Ensures the text is on top of the orb
+                    }}
+                >
+                    <GradientText
+                        colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+                        animationSpeed={3}
+                        showBorder={false}
+                        className="custom-class"
+                    >
+                        chromesthesia
+                    </GradientText>
+                </div>
+            </div>
+
+            {/* Bottom section: Dock */}
+
+
+
         </div>
         <Dock
             items={items}
-            panelHeight={40}
-            baseItemSize={80}
-            magnification={100}
+            panelHeight={25}
+            baseItemSize={50}
+            magnification={70}
         />
     </React.StrictMode>
 );
