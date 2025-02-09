5d997a65b7a9fa34c82a1606d4558dd99a9d2d7c
Victor <victoryang2005@outlook.com>
2025-02-09 00:03:54 -0800

    Routes

--- BCHacks/main/src/index.js
+++ BCHacks/main/src/index.js
@@ -3,20 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Dock from './blocks/Components/Dock/Dock';
-import { HiOutlineHome } from "react-icons/hi";
-import { HiOutlineCog } from "react-icons/hi";
-import { HiOutlineFolder } from "react-icons/hi";
-import { HiOutlineDocument } from "react-icons/hi";
+import { HiOutlineHome, HiOutlineCog, HiOutlineFolder, HiOutlineDocument } from "react-icons/hi";
 import LandingPage from "./LandingPage";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from 'react-router-dom';
 
 
 
 const items = [
-        { icon: <HiOutlineHome color={'white'} size={28}/>, label: 'Home', onClick: () => alert('Home!') },
+        { icon: <HiOutlineHome color={'white'} size={28}/>, label: 'Home', onClick: () => Go(<LandingPage />) },
         { icon: <HiOutlineDocument color={"white"} size={28}/>, label: 'Canvas', onClick: () => alert('Archive!') },
         { icon: <HiOutlineFolder color={"white"} size={28}/>, label: 'Gallery', onClick: () => alert('Profile!') },
         { icon: <HiOutlineCog color={"white"} size={28}/>, label: 'Settings', onClick: () => alert('Settings!') },
     ];
+
+function Go(element) {
+    <Router>
+        <Routes>
+            <Route path="/" element={element}/>
+        </Routes>
+    </Router>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
